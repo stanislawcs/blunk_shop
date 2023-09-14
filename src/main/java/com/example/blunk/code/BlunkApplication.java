4e1691a60e061b47075b649464a6797cca5f1bed
Stanislaw Shukan <shukans588@gmail.com>
2023-09-14 22:17:50 +0300

    Create requirements file

---
+++ src/main/java/com/example/blunk/code/BlunkApplication.java
@@ -0,0 +1,13 @@
+package com.example.blunk.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlunkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlunkApplication.class, args);
+    }
+
+}
